d0f76eafcd29ccb65e81ce7ee20bb45c85f0f663
Mahdi Bouaziz <mahdiibouaziz@gmail.com>
2024-12-27 20:26:30 +0100

    feat: hello world dummy contribution

---
+++ staging/src/k8s.io/kubectl/pkg/cmd/helloworld/helloworld.go
@@ -0,0 +1,49 @@
+package helloworld
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"k8s.io/cli-runtime/pkg/genericiooptions"
+
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+	"k8s.io/kubectl/pkg/util/i18n"
+	"k8s.io/kubectl/pkg/util/templates"
+)
+
+var (
+	helloworlExample = templates.Examples(`
+		# Print Hello World
+		kubectl hello-world
+	`)
+)
+
+type HelloWorldOptions struct {
+	genericiooptions.IOStreams
+}
+
+func NewHelloWorldOptions(ioStreams genericiooptions.IOStreams) *HelloWorldOptions {
+	return &HelloWorldOptions{
+		IOStreams: ioStreams,
+	}
+}
+
+func NewCmdHelloWorld(f cmdutil.Factory, ioStreams genericiooptions.IOStreams) *cobra.Command {
+	// o := NewHelloWorldOptions(ioStreams)
+
+	cmd := &cobra.Command{
+		Use:     "hello-world",
+		Short:   i18n.T("Print hello world"),
+		Long:    i18n.T("Print hello world."),
+		Example: helloworlExample,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello World")
+			// cmdutil.CheckErr(o.Complete(restClientGetter, cmd, args))
+			// cmdutil.CheckErr(o.Validate())
+			// cmdutil.CheckErr(o.RunHelloWorld())
+		},
+	}
+
+	return cmd
+}
